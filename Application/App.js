87831c7372524fe5e831959e9a8a0e3f9ca6bf99
quangtran <anhquangepic@gmail.com>
2018-12-20 13:53:19 +0700

    okta server

--- Application/App.js
+++ Application/App.js
@@ -1,60 +1,153 @@
-import React from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
-import Main from './components/Main.js';
-import Login from './components/Login.js';
-import Add from './components/Add.js';
+import React, { Component } from 'react';
+import { Alert, UIManager, LayoutAnimation } from 'react-native';
+import { authorize, refresh, revoke } from 'react-native-app-auth';
+import { Page, Button, ButtonContainer, Form, Heading } from './components';
+import { Buffer } from 'buffer';
 
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
 
-export default class App extends React.Component {
+type State = {
+  hasLoggedInOnce: boolean,
+  accessToken: ?string,
+  accessTokenExpirationDate: ?string,
+  refreshToken: ?string,
+  idToken: ?string,
+  beers: []
+};
+
+const config = {
+  issuer: 'https://dev-737523.oktapreview.com/oauth2/default',
+  clientId: '0oagessy4mUlMTXW70h7',
+  redirectUrl: 'com.oktapreview.dev-737523:/callback',
+  additionalParameters: {},
+  scopes: ['openid', 'profile', 'email', 'offline_access']
+};
+
+export default class App extends Component<{}, State> {
   state = {
-    username: '',
-    password: '',
-    isLoginIn: true,
-    state: 'Login',
-    devices: ["+"],
+    hasLoggedInOnce: false,
+    accessToken: '',
+    accessTokenExpirationDate: '',
+    refreshToken: '',
+    idToken: '',
+    beers: []
+  };
+
+  animateState(nextState: $Shape<State>, delay: number = 0) {
+    setTimeout(() => {
+      this.setState(() => {
+        LayoutAnimation.easeInEaseOut();
+        return nextState;
+      });
+    }, delay);
   }
-  render() {
-    switch(this.state.state) {
-      case 'Login':
-        return (
-          <Login
-                onLoginPress={(username) => this.setState({state: 'Main', username})}
-            />
-        );
-      case 'Main':
-        return (
-            <Main
-                onLoginPress={() => this.setState({state: 'Login'})}
-                onAddPress={() => this.setState({state: 'Add'})}
-                username={this.state.username}
-                devices={this.state.devices}
-            />
-        );
-      case 'Add':
-        return (
-            <Add
-                onLoginPress={() => this.setState({state: 'Main'})}
-                updateDevices={(devices) => this.setState(devices)}
-                username={this.state.username}
-                devices={this.state.devices}
-            />
-        );
-      default:
-        return (
-          <View style={styles.container}>
-            <Text> Unknow State </Text>
-          </View>
-        );
+
+  authorize = async () => {
+    try {
+      const authState = await authorize(config);
+      this.animateState(
+        {
+          hasLoggedInOnce: true,
+          accessToken: authState.accessToken,
+          accessTokenExpirationDate: authState.accessTokenExpirationDate,
+          refreshToken: authState.refreshToken,
+          idToken: authState.idToken
+        },
+        500
+      );
+    } catch (error) {
+      Alert.alert('Failed to log in', error.message);
     }
+  };
+
+  refresh = async () => {
+    try {
+      const authState = await refresh(config, {
+        refreshToken: this.state.refreshToken
+      });
+
+      this.animateState({
+        accessToken: authState.accessToken || this.state.accessToken,
+        accessTokenExpirationDate:
+          authState.accessTokenExpirationDate || this.state.accessTokenExpirationDate,
+        refreshToken: authState.refreshToken || this.state.refreshToken
+      });
+    } catch (error) {
+      Alert.alert('Failed to refresh token', error.message);
+    }
+  };
+  
+  fetchGoodBeers = async () => {
+    if (this.state.beers.length) {
+      // reset to id token if beers is already populated
+      this.animateState({beers: []})
+    } else {
+      try {
+        const response = await fetch('http://192.168.0.60:8080/good-beers', {
+          headers: {
+            'Authorization': `Bearer ${this.state.accessToken}`
+          }
+        });
+        const data = await response.json();
+        this.animateState({beers: data});
+      } catch(error) {
+        console.error(error);
+      }
+    }
+  };
+
+  revoke = async () => {
+    try {
+      await revoke(config, {
+        tokenToRevoke: this.state.accessToken,
+        sendClientId: true
+      });
+      this.animateState({
+        accessToken: '',
+        accessTokenExpirationDate: '',
+        refreshToken: '',
+        beers: []
+      });
+    } catch (error) {
+      Alert.alert('Failed to revoke token', error.message);
+    }
+  };
+
+  render() {
+    const {state} = this;
+    if (state.idToken) {
+      const jwtBody = state.idToken.split('.')[1];
+      const base64 = jwtBody.replace('-', '+').replace('_', '/');
+      const decodedJwt = Buffer.from(base64, 'base64');
+      state.idTokenJSON = JSON.parse(decodedJwt);
+    }
+    return (
+      <Page>
+        {!!state.accessToken ? (
+          <Form>
+            <Form.Label>accessToken</Form.Label>
+            <Form.Value>{state.accessToken}</Form.Value>
+            <Form.Label>{state.beers.length ? 'Good Beers' : 'ID Token'}</Form.Label>
+            <Form.Value>{JSON.stringify(state.beers.length ? state.beers : state.idTokenJSON)}</Form.Value>
+            <Form.Label>accessTokenExpirationDate</Form.Label>
+            <Form.Value>{state.accessTokenExpirationDate}</Form.Value>
+            <Form.Label>refreshToken</Form.Label>
+            <Form.Value>{state.refreshToken}</Form.Value>
+          </Form>
+        ) : (
+          <Heading>{state.hasLoggedInOnce ? 'Goodbye.' : 'Hello, stranger.'}</Heading>
+        )}
+
+        <ButtonContainer>
+          {!state.accessToken && (
+            <Button onPress={this.authorize} text="Authorize" color="#017CC0"/>
+          )}
+          {!!state.refreshToken && <Button onPress={this.refresh} text="Refresh" color="#24C2CB"/>}
+          {!!state.accessToken && <Button onPress={this.revoke} text="Revoke" color="#EF525B"/>}
+          {!!state.accessToken && <Button onPress={this.fetchGoodBeers} text={!this.state.beers.length ? 'Good Beers' : 'ID Token'} color="#008000" />}
+        </ButtonContainer>
+      </Page>
+    );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#3498db'
-  },
-});
